03a4141d455119e0507921458e46e002c354febe
AlexLkv <83674613+AlexLkv@users.noreply.github.com>
2025-03-20 01:33:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
     res.send("Сервер работает");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 server.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
